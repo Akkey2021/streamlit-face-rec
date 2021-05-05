59d8485fbc19546286546ff03c8e3c8e4b2a97db
Akkey2021 <83562814+Akkey2021@users.noreply.github.com>
2021-05-05 10:41:03 +0900

    Create 210505.py

---
+++ 210505.py
@@ -0,0 +1,44 @@
+import streamlit as st
+from PIL import Image
+import requests
+from PIL import ImageDraw
+import io
+
+subscription_key="98c0050da5aa48408a43ff9d91076155"
+assert subscription_key
+face_api_url = 'https://20210503ak-python.cognitiveservices.azure.com/face/v1.0/detect'
+
+st.title("Face Recognition App")
+uploaded_file=st.file_uploader("choose an image…", type="jpg")
+if uploaded_file is not None:
+    #uploaded_fileが入っていたら、、、という意味のIf文
+    img = Image.open(uploaded_file)
+    with io.BytesIO() as output:
+        img.save(output, format='JPEG')
+        binary_img=output.getvalue() #バイナリの取得
+
+    headers = {
+    'Content-Type':'application/octet-stream',
+    'Ocp-Apim-Subscription-Key': subscription_key}
+
+    params = {
+    'returnFaceId': 'true',
+    'returnFaceAttributes':'age,gender,emotion,smile,hair'
+    }
+
+    res = requests.post(face_api_url, params=params,headers=headers, data=binary_img)
+
+
+    results= res.json()
+
+    for result in results:
+        rect=result['faceRectangle']
+
+        draw = ImageDraw.Draw(img)
+        draw.rectangle(
+            [
+                rect['left'],rect['top'],
+                rect['left']+rect['width'],rect['top']+rect['height'] #rectangleは左上と右下の座標だけでOK
+            ],fill=None, outline='red', width=5)
+        
+    st.image(img, caption="uploaded image", use_column_width=True)
